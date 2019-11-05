97cfb174239a0ad98424b144f4b92c6b122f8b3f
Erik White <26148654+Erik-White@users.noreply.github.com>
2019-11-05 10:31:55 +0200

    Update to version 0.0.3

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def read(*names, **kwargs):
 setup(
     python_requires = ">3.7",
     name = "actions_test_python",
-    version = "0.0.2",
+    version = "0.0.3",
     description = "Testing out GitHub actions with a minimal Python package",
     long_description = "%s\n%s" % (
         re.compile("^.. start-badges.*^.. end-badges", re.M | re.S).sub("", read("README.md")),
